3ff719071e334ac6ddfd578819e8c4ff7eebc030
Frederic Bour <frederic.bour@lakaban.net>
2014-04-30 15:37:09 +0100

    Separate generalize_implicits from generalize
    Choose specific implicits generalization points

--- typing/ctype.ml
+++ typing/ctype.ml
@@ -17,8 +17,6 @@ open Asttypes
 open Types
 open Btype
 
-let generalize_implicits = ref (fun () -> assert false)
-
 (*
    Type manipulation after type inference
    ======================================
@@ -639,7 +637,6 @@ let iter_generalize tyl ty =
   iter_generalize tyl ty
 
 let generalize ty =
-  !generalize_implicits ();
   iter_generalize (ref []) ty
 
 (* Efficient repeated generalisation of the same type *)
