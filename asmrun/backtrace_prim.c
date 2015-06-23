f8e385db030119c9f10ec4a9a287e7f7f92200af
Frédéric Bour <frederic.bour@lakaban.net>
2015-06-23 09:22:28 +0100

    retloc -> getcaller

--- asmrun/backtrace_prim.c
+++ asmrun/backtrace_prim.c
@@ -227,11 +227,11 @@ int caml_debug_info_available(void)
   return 1;
 }
 
-/* Turn encoded retaddr into eventual location information */
-CAMLprim value caml_decode_retaddr(value retaddr)
+/* Turn encoded retaddr into a raw_backtrace_slot */
+CAMLprim value caml_caller_slot(value retaddr)
 {
   CAMLparam1(retaddr);
-  CAMLlocal3(result, block, fname);
+  CAMLlocal2(result, slot);
 
   frame_descr *d;
   uintnat h;
@@ -239,8 +239,7 @@ CAMLprim value caml_decode_retaddr(value retaddr)
 
   if (caml_frame_descriptors == NULL) caml_init_frame_descriptors();
 
-  if (Is_long(retaddr) && retaddr > 1)
-  {
+  if (Is_long(retaddr) && retaddr > 1) {
     /* Find the descriptor corresponding to the return address */
     h = Hash_retaddr(retaddr);
     while(1) {
@@ -253,23 +252,16 @@ CAMLprim value caml_decode_retaddr(value retaddr)
       h = (h+1) & caml_frame_descriptors_mask;
     }
 
-    if (d != NULL)
-      extract_location_info(d, &li);
-
-    if (d != NULL && li.loc_valid) {
-      fname = caml_copy_string(li.loc_filename);
-      block = caml_alloc_small(3, 0);
-      Field(block, 0) = fname;
-      Field(block, 1) = Val_int(li.loc_lnum);
-      Field(block, 2) = Val_int(li.loc_startchr);
+    if (d != NULL) {
+      slot = caml_val_raw_backtrace_slot(d);
       result = caml_alloc_small(1, 0);
-      Field(result, 0) = block;
+      Field(result, 0) = slot;
     } else {
       result = Val_unit;
     }
+  } else {
+    result = Val_unit;
   }
-  else
-    result = retaddr;
 
   CAMLreturn(result);
 }
