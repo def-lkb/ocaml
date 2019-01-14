281bb3793caaf022757cf0eb76e1aebaac2afd77
Frédéric Bour <def@fb.com>
2019-01-14 14:44:37 +0100

    debugger printval: handle depth and cycles

--- debugger/printval.ml
+++ debugger/printval.ml
@@ -91,6 +91,8 @@ let descriptors = lazy (
   index
 )
 
+module H = Hashtbl.Make(struct type t = Obj.t let equal = (==) let hash = Hashtbl.hash end)
+
 let opaque_printer _kind obj =
   match Obj.repr (Debugcom.Remote_value.obj obj) with
   | exception Debugcom.Marshalling_error ->
@@ -103,38 +105,48 @@ let opaque_printer _kind obj =
         done;
         !acc
       in
+      let table = H.create 7 in
       let rec print depth obj =
-        let open Tagprinter.Introspect in
-        let open Outcometree in
-        match
-          Tagprinter.Introspect.tagged_dynval (Lazy.force descriptors) obj
-        with
-        | String str ->
-          Oval_string (str, 70, Ostr_string)
-        | Float f -> Oval_float f
-        | Int_or_constant (n, []) -> Oval_int n
-        | Char n -> Oval_char n
-        | Int_or_constant (n, name :: _) ->
-          Oval_stuff (Printf.sprintf "%d or `%s" n name)
-        | Constant names ->
-          Oval_stuff (String.concat " or " names)
-        | Array fields ->
-          Oval_array (list_fields (print (depth - 1)) fields)
-        | Tuple {name; fields} ->
-          let tuple = list_fields (print (depth - 1)) fields in
-          Oval_constr (Oide_ident name, tuple)
-        | Record {name; fields} ->
-          let pf (k,v) = (Oide_ident k, print (depth - 1) v) in
-          let args = Oval_record (list_fields pf fields) in
-          Oval_constr (Oide_ident name, [args])
-        | Polymorphic_variant (name, tuple) ->
-            let args = Tagprinter.Introspect.no_approx tuple in
-            Oval_variant (name, Some (print (depth - 1) args))
-        | Closure  -> Oval_stuff "<closure>"
-        | Lazy     -> Oval_stuff "<lazy>"
-        | Abstract -> Oval_stuff "<abstract>"
-        | Custom   -> Oval_stuff "<custom>"
-        | Unknown  -> Oval_stuff "<unknown>"
+        if depth <= 0 then
+          Outcometree.Oval_ellipsis
+        else if H.mem table (snd obj) then
+          Outcometree.Oval_stuff "<cycle>"
+        else
+          let open Tagprinter.Introspect in
+          let open Outcometree in
+          match
+            Tagprinter.Introspect.tagged_dynval (Lazy.force descriptors) obj
+          with
+          | String str ->
+              Oval_string (str, 70, Ostr_string)
+          | Float f -> Oval_float f
+          | Int_or_constant (n, []) -> Oval_int n
+          | Char n -> Oval_char n
+          | Int_or_constant (n, name :: _) ->
+              Oval_stuff (Printf.sprintf "%d or `%s" n name)
+          | Constant names ->
+              Oval_stuff (String.concat " or " names)
+          | Array fields ->
+              H.add table (snd obj) ();
+              Oval_array (list_fields (print (depth - 1)) fields)
+          | Tuple {name; fields} ->
+              H.add table (snd obj) ();
+              let tuple = list_fields (print (depth - 1)) fields in
+              Oval_constr (Oide_ident name, tuple)
+          | Record {name; fields} ->
+              H.add table (snd obj) ();
+              let pf (k,v) = (Oide_ident k, print (depth - 1) v) in
+              let args = Oval_record (list_fields pf fields) in
+              Oval_constr (Oide_ident name, [args])
+          | Polymorphic_variant (name, tuple) ->
+              H.add table (snd obj) ();
+              let args = Tagprinter.Introspect.no_approx tuple in
+              Oval_variant (name, Some (print (depth - 1) args))
+          | Closure  -> Oval_stuff "<closure>"
+          | Lazy     -> Oval_stuff "<lazy>"
+          | Abstract -> Oval_stuff "<abstract>"
+          | Custom   -> Oval_stuff "<custom>"
+          | Unknown  -> Oval_stuff "<unknown>"
       in
       Some (print !max_printer_depth (Tagprinter.Introspect.no_approx obj))
 
